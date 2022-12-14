6468ca4cb165ee3612eff8733aa2a9eeecd5cbdb
whale0928 <rlagusrl928@gmail.com>
2022-12-14 23:23:49 +0900

    feat : Create Default Setting

---
+++ src/main/java/helloJpa/Main.java
@@ -0,0 +1,12 @@
+package helloJpa;
+
+import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
+
+import javax.persistence.EntityManager;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
