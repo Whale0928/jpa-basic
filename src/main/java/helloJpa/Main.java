6f990978db1bf7f06cdf080e794a4ed4cf291278
KIM HYUN GI <rlagusrl928@gmail.com>
2023-04-23 15:56:48 +0900

    chore : maven => gradle 변경

--- src/main/java/helloJpa/Main.java
+++ src/main/java/helloJpa/Main.java
@@ -1,12 +1,7 @@
 package helloJpa;
 
-import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
-
-import javax.persistence.EntityManager;
-
 public class Main {
-    public static void main(String[] args) {
-
-
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello JPA");
+	}
 }
