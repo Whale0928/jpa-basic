d256e447bd943eec115f61e65f48bef2408b791b
KIM HYUN GI <rlagusrl928@gmail.com>
2023-04-23 16:14:26 +0900

    feat : JPA Entity Manager 최소 생성 및 연결 테스트

--- src/main/java/helloJpa/Main.java
+++ src/main/java/helloJpa/Main.java
@@ -1,7 +1,21 @@
 package helloJpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello JPA");
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+		//코드 영역 시작
+
+		//코드 영역 끝
+
+		em.clear();
+		entityManagerFactory.close();
 	}
 }
